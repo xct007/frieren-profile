eeb3fa4b551995d60dce6262f7d9e42a379ce410
FrierenDv <38551757+xct007@users.noreply.github.com>
2022-07-09 05:30:37 +0700

    Update skills.ts

--- src/pages/api/skills.ts
+++ src/pages/api/skills.ts
@@ -3,16 +3,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-	const AboutMe = [
+	const technologies = [
         'You can ask me about JavaScript',
 	'You can ask me about Python',
         'You can ask me about English Language',
 	'You can ask me about Anime',
 	'You can ask me about BlackPink',
 	'You can ask me about Anythings',
-	
 	];
 	res.status(200).json({
-		AboutMe
+		technologies
 	});
 };
