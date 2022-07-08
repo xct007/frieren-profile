7cfb0298240d3788bafbd7bd4d57e44d7fe6f415
FrierenDv <38551757+xct007@users.noreply.github.com>
2022-07-09 05:15:10 +0700

    simple

--- src/pages/api/skills.ts
+++ src/pages/api/skills.ts
@@ -3,52 +3,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-	const technologies = [
-        'You can ask me about Python',
-        'You are welcome to inquire about the English language',
-		`You can ask me about anime if you're a weeb`,
-		'Also you can ask me about manga',
-        'You can ask me about blackPink',
-        'You can ask me about love',
-        "You can inquire as to why I am alone",
-        "You are welcome to inquire as to why I am lonely",
-		'If you have any questions concerning JavaScript, please do not hesitate to contact me',
-        "Ask ask ask ask anythings"
+	const AboutMe = [
+        'You can ask me about JavaScript',
+	'You can ask me about Python',
+        'You can ask me about English Language',
+	'You can ask me about Anime',
+	'You can ask me about BlackPink',
+	'You can ask me about Anythings',
+	
 	];
-    const greetings = ['mate']
-	const frameworks = ['Laravel', 'Vue.js', 'Nuxt.js'];
-	const databases = ['MySQL', 'PostgreSQL', 'Firebase'];
-	const cms = ['WordPress', 'Statamic'];
-	const various = ['SEO', 'Scrum', 'Agile', 'Jira', 'Confluence'];
-	const design = ['Canva', 'Gravit', 'Figma'];
-	const softSkills = [
-		'Leadership',
-		'Problem solving',
-		'Creative thinking',
-		'Flexible and adaptive',
-		'Team player',
-	];
-	const languages = [
-		{
-			language: 'Indonesian',
-			level: 'bilingual',
-			icon: '🇮🇩',
-		},
-		{
-			language: 'English',
-			level: 'basic',
-			icon: '🇬🇧',
-		},
-	];
-
 	res.status(200).json({
-		technologies,
-		frameworks,
-		databases,
-		cms,
-		various,
-		design,
-		softSkills,
-		languages,
+		AboutMe
 	});
 };
